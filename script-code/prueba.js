081868fd92d297d09980185b721bc25798b3a37d
mati romero <matias.1993.romero@gmai.com>
2020-10-01 13:36:59 -0300

    Nuevos cambios

--- script-code/prueba.js
+++
@@ -1,7 +0,0 @@
-var imegn = document.getElementById("imagen");
-var box = document.getElementById("box");
-
-imagen.addEventListener('mouseover', function() {
-	imagen.classList.toggle('img-no-show');
-	box.classList.toggle('box-show');
-});
