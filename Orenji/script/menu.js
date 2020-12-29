ea958f37f72e3e69866e590428948db2ef298360
mati romero <matias.1993.romero@gmail.com>
2020-12-28 22:30:02 -0300

    Nuevo CV

--- Orenji/script/menu.js
+++
@@ -1,6 +0,0 @@
-var menu = document.getElementById("menu");
-var nav = document.getElementById("nav");
-
-menu.addEventListener('click', function() {
-	nav.classList.toggle('navigation-nav-show');
-});
