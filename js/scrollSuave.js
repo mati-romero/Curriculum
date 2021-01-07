cbe88975e4f5736b1fe1b949e5016ae1e31f1673
mati romero <matias.1993.romero@gmail.com>
2021-01-06 23:17:33 -0300

    Efectos de Scroll

---
+++ js/scrollSuave.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+
+  // Mostrar y ocultar botón "Volver arriba"
+
+  var btnVolverArriba = $('#btnVolverArriba');
+
+  $(window).on('scroll', function () {
+
+    var SobreMiOffsetTop = $('#Sobre-mi').offset().top;
+
+    if ($(window).scrollTop() >= SobreMiOffsetTop) {
+
+      btnVolverArriba.css('margin-right', 0);
+
+    }else if ($(window).scrollTop() <= SobreMiOffsetTop/2) {
+
+      btnVolverArriba.css('margin-right', '-60px');
+
+    }
+
+  })
+
+  // Movimiento suave de scroll de "Inicio" y "Volver arriba"
+
+  $('a.volver-arriba').on('click', function (e) {
+    e.preventDefault();
+
+    if ($(window).scrollTop() != 0) {
+      $('html, body').stop().animate({scrollTop: 0}, 1000);
+    }
+
+  })
+
+  // Movimiento suave de scroll de los demás items del menú principal
+
+  $('a.scroll-suave').on('click', function (e) {
+    e.preventDefault();
+    var seccionOffsetTop = $($(this).attr('href')).offset().top
+    $('html, body').stop().animate({scrollTop: seccionOffsetTop}, 1000);
+  });
+
+  $('a.scroll-acercaDe').on('click', function (e) {
+    e.preventDefault();
+    var seccionOffsetTop = $($(this).attr('href')).offset().top + 100;
+    $('html, body').stop().animate({scrollTop: seccionOffsetTop}, 1000);
+  });
+
+});
